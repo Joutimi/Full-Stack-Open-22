88678465e3265adc9e568509d381cde90829af22
Joutimi <timijouppila@hotmail.com>
2022-07-25 15:56:01 +0300

    1.5 push

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,50 +1,57 @@
 //App
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
 //Komponentit
+  const parts = course.parts //partsit irti coursesta
+  console.log(parts)
+
   const Header = (props) => {
     return (
-      <h1> {course} </h1>
+      <h1> {props.course} </h1>
     )}
 
   const Part = (props) => {
     return (
-        <p>{props.part} {props.exercises} </p>
+        <p>{props.name} {props.exercises} </p>
     )}
 
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={part1.name} exercises={part1.exercises} />
-        <Part part={part2.name} exercises={part2.exercises} />
-        <Part part={part3.name} exercises={part3.exercises} />
-      </div>
+  const Content = () => {
+      return (
+        <div>
+          <Part name={parts[0].name} exercises={parts[0].exercises} />
+          <Part name={parts[1].name} exercises={parts[1].exercises} />
+          <Part name={parts[2].name} exercises={parts[2].exercises} />
+        </div>
     )}
 
-  const Total = (props) => {
-    return (
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises} </p> 
+  const Total = () => {
+      return (
+        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises} </p> 
     )}
 
 //Return
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course.name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )}
 
