141909d250b8a5102b783c2c7285f0413bbfadd6
Joutimi <Timijouppila@hotmail.com>
2022-12-29 16:35:15 +0200

    Axios asennettu, json-server deviasennus, Appi toimii; hakee portista 3001, jsonista tiedot

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import AddNewNumber from './components/AddNewNumber'
 import { NumbersList } from './components/NumbersList'
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  
+  console.log('render', persons.length, 'persons')
+  
 
   //tilat
   const [newName, setNewName] = useState('')
--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import axios from 'axios'
+const promise = axios.get('http:/localhost:3001/persons')
+console.log(promise)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
